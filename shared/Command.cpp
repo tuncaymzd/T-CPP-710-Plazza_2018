fd8b3d3df657ae192d8aadbb7a197beb5461e45f
Turkoglu Tuncay <tuncay.turkoglu@etudiant.univ-lille1.fr>
2018-11-05 18:15:52 +0100

    Command class finished

---
+++ shared/Command.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Tuncay Turkoglu on 05/11/2018.
+//
+
+#include "include/Command.h"
+
+Command::Command(std::string name, std::string size) {
+    this->namePizza = name;
+    this->size = size;
+}
+
