13bec53555c1838cb77daa9ec17a4921493821f2
Turkoglu Tuncay <tuncay.turkoglu@etudiant.univ-lille1.fr>
2018-11-07 12:42:30 +0100

    avancement sur le controleur

--- Controleur.cpp
+++ Controleur.cpp
@@ -2,4 +2,51 @@
 // Created by Tuncay Turkoglu on 06/11/2018.
 //
 
-#include "Controleur.hpp"
+#include "include/Controleur.hpp"
+
+Controleur::Controleur() {
+    this->parserConf = new ParserConfig();
+    this->pars = new Parser();
+}
+
+void Controleur::addCommand(Command c) {
+    this->theCommands.push(c);
+}
+
+bool Controleur::theCommandsIsEmpty() {
+    return this->theCommands.empty();
+}
+
+void Controleur::deleteCommand() {
+    if (this->theCommandsIsEmpty()) {
+        throw Error("The commands pile is empty");
+    }
+    this->theCommands.pop();
+}
+
+void Controleur::readFile(std::string file) {
+    std::string line;
+    std::tuple<std::string, std::string, int> cmd;
+    std::string pizzaName;
+    std::string size;
+    int nb = 0;
+    int i = 0;
+
+    this->file.open(file);
+    if (!this->file.is_open()) {
+        throw Error("This file can not be opened");
+    }
+    while (this->file.getline(file, line)) {
+        cmd = this->pars.parse(line);
+        pizzaName = std::get<0>(cmd);
+        size = std::get<1>(cmd);
+        nb = std::get<2>(cmd);
+        while (i < nb) {
+            this->addCommand(new Command(pizzaName, size));
+            i++;
+        }
+        i = 0;
+    }
+
+    this->file.close();
+}
--- include/Controleur.hpp
+++ include/Controleur.hpp
@@ -5,12 +5,41 @@
 #ifndef PLAZZA_CONTROLEUR_HPP
 #define PLAZZA_CONTROLEUR_HPP
 
+#include <queue>
+#include <iostream>
+#include "../shared/Command.cpp"
+#include "../ParserConfig.cpp"
+#include <fstream>
+#include "../Error.cpp"
+#include <tuple>
+#include <get>
 
+//inclure parser.cpp
 
 class Controleur {
+public:
+    static int baseTime;
+    static int numbersOfKitchens;
+    static int numbersOfCooker;
 
+    Controleur();
+    ~Controleur();
+
+    void readFile(std::string file);
+
+    void addCommand(Command c);
+
+    void deleteCommand();
+
+    bool theCommandsIsEmpty();
+
+
+private:
+    std::queue<Command> theCommands;
+    std::fstream file;
+    ParserConfig *parserConf;
+    Parser *pars;
 };
-
 
 
 #endif //PLAZZA_CONTROLEUR_HPP
