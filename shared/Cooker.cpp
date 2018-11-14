e2964f135591da0f95049bb6a059e1f0b5b23fa7
damiendoumer <damientohin@gmail.com>
2018-11-14 13:55:27 +0100

    Finished basic implementation of thread pool

--- shared/Cooker.cpp
+++ shared/Cooker.cpp
@@ -5,14 +5,17 @@
 #include <chrono>
 #include <thread>
 #include "includes/Cooker.h"
+#include "includes/Kitchen.h"
 
 void Cooker::cook() {
     onNotify("Pizza: " + pizza.getName() + " " + " " + pizza.getSize() +
     " has started cooking and will last " + std::to_string(pizza.getCookingTime()) + " Miliseconds.\n");
     std::this_thread::sleep_for (std::chrono::milliseconds(pizza.getCookingTime()));
+    finish();
 }
 
 void Cooker::finish() {
     onNotify("Pizza: " + pizza.getName() + " " + " " + pizza.getSize() +
              " has finished Cooking!\n");
+    Kitchen::threadTerminated();
 }
--- shared/includes/Kitchen.h
+++ shared/includes/Kitchen.h
@@ -9,24 +9,30 @@
 #include <mutex>
 #include "ICook.h"
 #include "Delegates.h"
+#include "Command.h"
 #include <queue>
 #include <thread>
 
 class Kitchen {
-
     static bool cooking;
     static int myProcessesID;
     static int parentProcessesID;
+    static long time;
     static notifierEventHandler onNotify;
     static std::queue<ICook*> cookQueue;
     static std::vector<std::thread*> threadPool;
+    static std::mutex cookerMutex;
 
 public:
     static void killMe();
     static void hangMeUp();
     static void setOnNotify(notifierEventHandler notif);
     static void run();
-    static void setCooker(std::queue<ICook*> cookQ);
+    static void Initialize(long time, std::queue<Command> pizzaCommands, int cmdNum);
+    static void setCookers(std::queue<ICook*> cookQ);
+    static void threadTerminated();
+    static void initializeProcessIds(int parentId, int myId);
+    static void notifyFinished();
 };
 
 #endif //PLAZZA_KITCHEN_H
